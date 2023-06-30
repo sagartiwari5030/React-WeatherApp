6e94b2905d483a7a120189f81484882e1a73754f
Sagar Tiwari <61117989+sagartiwari5030@users.noreply.github.com>
2023-06-30 13:08:09 +0530

    React-weatherApp

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "74c1e4972578cf8841154b2763739f95",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
